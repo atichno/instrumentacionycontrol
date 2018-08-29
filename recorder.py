908d915310f8bba0bda28ddc0b6d5b294e3bec40
lucaspavlov <lucaspavlov@gmail.com>
2018-08-29 19:04:07 -0300

    Agregamos barrido en frecuencias y función escalón
    Se armó una función escalón (que finalmente encontramos que no tenía mucha utilidad ya que la placa no funciona en continua).
    Se armó un barrido en frecuencia usando un for loop.

--- recorder.py
+++ recorder.py
@@ -17,6 +17,12 @@ Non-blocking mode (start and stop recording):
 
 import pyaudio
 import wave
+import numpy as np
+import wave
+from time import strftime
+import glob
+import os
+import matplotlib.pyplot as plt
 import time
 
 class Recorder(object):
@@ -97,12 +103,23 @@ class RecordingFile(object):
         wavefile.setframerate(self.rate)
         return wavefile
     
+     # genero escalones
+def escalon(n_escalones,long_escalon,desde,hasta):
+    samples = np.linspace(0,1,n_escalones * long_escalon)
+    samples = samples * n_escalones # solo sirve si linspace va de 0 a 1
+    samples = np.floor(samples)
+    samples = samples / n_escalones
+    samples = desde + (hasta - desde) * samples    
+    return samples
     
-
-import numpy as np
-
+# %%
+os.chdir("C:\\Users\\Publico\\Desktop\\Instrumentacion\\instrumentacionycontrol\\Prueba recorder\\")
+## Tomamos dia y hora actual para dar nombre al archivo wav
+#mes_dia = strftime("%m%d-%H_%M_%S")
+# Agregamos un contador para facilitar referir a los archivos
+num_wavs = len(glob.glob(os.path.join(os.getcwd(), '*.wav')))
 rec = Recorder(channels=2)
-with rec.open('nonblocking.wav', 'wb') as recfile2:
+with rec.open('medicion-{}.wav'.format(num_wavs+1), 'wb') as recfile2:
     recfile2.start_recording()
     #time.sleep(10.0)
     CHUNK = 1024
@@ -111,29 +128,54 @@ with rec.open('nonblocking.wav', 'wb') as recfile2:
     #    print("Plays a wave file.\n\nUsage: %s filename.wav" % sys.argv[0])
     #    sys.exit(-1)
     #wf = wave.open('output.wav', 'rb')
-    volume = 1    # range [0.0, 1.0]
+    volume = 0.51    # range [0.0, 1.0]
     fs = 44100       # sampling rate, Hz, must be integer
-    duration = 5.0   # in seconds, may be float
-    f = 200      # sine frequency, Hz, may be float
-    samples = volume*(np.cos(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
-    #samples = volume*(np.cos(2*np.pi*np.arange(fs*duration))).astype(np.float32)
-    p = pyaudio.PyAudio()
+    duration = 1.0 # in seconds, may be float
+    f = 2000      # sine frequency, Hz, may be float
+ #   samples = volume*(np.cos(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
     
-    stream = p.open(format=pyaudio.paFloat32,
+    # Barriendo en frecuencia
+    frecuencia_inicial = 10 ;
+    frecuencia_final = 20000 ;
+    salto = 1000;
+    frecuencias = np.arange(frecuencia_inicial,frecuencia_final,salto)
+    n_periodos = 5 # cantidad de periodos
+
+    for freq in frecuencias:
+        p = pyaudio.PyAudio()
+    
+        stream = p.open(format=pyaudio.paFloat32,
                     channels=1,
                     rate=fs,
                     output=True)
+#        duration = n_periodos/freq
+        duration = 0.5
+        senial = volume * (np.sin(2*np.pi*np.arange(fs*duration)*freq/fs)).astype(np.float32)
+        
+        fin = CHUNK
+        
+        while fin < len(senial):
+            data = senial[fin-CHUNK:fin].tobytes()
+            stream.write(data)
+            fin += CHUNK
+        stream.stop_stream()
+        stream.close()
     
-    fin = CHUNK
+        p.terminate()    
+        #samples = volume*(np.cos(2*np.pi*np.arange(fs*duration))).astype(np.float32)
+
     
-    while fin < len(samples):
-        data = samples[fin-CHUNK:fin].tobytes()
-        stream.write(data)
-        fin += CHUNK
-    
-    stream.stop_stream()
-    stream.close()
-    
-    p.terminate()
     
     recfile2.stop_recording()
+    
+# %% Graficar
+with wave.open('nonblocking.wav','r') as spf:
+    #Extract Raw Audio from Wav File
+    signal = spf.readframes(-1)
+    signal = np.frombuffer(signal, 'Int16')
+    fs = spf.getframerate()
+    tiempo = np.linspace(0, len(signal)/fs, num=len(signal))
+    plt.figure(1)
+    plt.title('Signal Wave...')
+    plt.plot(tiempo, signal)
+    plt.show()
