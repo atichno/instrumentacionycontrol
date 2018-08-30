3865f978b6ebda40057a6605eebbf0bd859de0b6
jfdoppler <dopplerjuan@gmail.com>
2018-08-30 01:46:11 -0300

    Estilo y barrido
    Mi toc me obligo a emprolijar el estilo.
    Aumento frecuencia de sampleo al maximo de mi placa.
    Cambio barrido a separacion logaritmica definiendo cantidad de frecuencias.
    Para generar las señales uso la funcion definida en commit previo.
    Ajusto la duracion de cada señal para que entren 10 periodos y optimizar el tiempo

--- recorder.py
+++ recorder.py
@@ -14,31 +14,42 @@ def escalon(n_escalones, long_escalon, desde, hasta):
     Genera una señal escalonada entre dos valores
     """
     samples = np.linspace(0, 1, n_escalones * long_escalon)
-    samples = samples * n_escalones # solo sirve si linspace va de 0 a 1
+    samples *= n_escalones  # solo sirve si linspace va de 0 a 1
     samples = np.floor(samples)
     samples = samples / n_escalones
-    samples = desde + (hasta - desde) * samples    
+    samples = desde+(hasta-desde)*samples
     return samples
 
 
-def senoidal(f_sampleo=44100, frecuencia=10, duracion=1., vpp=1.):
+def senoidal(f_sampleo=44100, frecuencia=10, duracion=1., vpp=1.,
+             dtype=np.float32):
     """
     Genera una señal senoidal de frecuencia y duracion definida
     """
-    return 2*vpp*(np.sin(2*np.pi*np.arange(f_sampleo*duracion)*frecuencia/f_sampleo))
+    times = np.arange(f_sampleo*duracion)
+    return 2*vpp*(np.sin(2*np.pi*times*frecuencia/f_sampleo)).astype(dtype)
 
 
-def cuadrada(f_sampleo=44100, frecuencia=10, duracion=1., minimo=0., maximo=1.):
+def cuadrada(f_sampleo=44100, frecuencia=10, duracion=1., minimo=0.,
+             maximo=1.):
     señal = senoidal(f_sampleo=f_sampleo, frecuencia=frecuencia,
                      duracion=duracion)
     return (np.sign(señal)+minimo+1)/(2*(maximo-minimo))
 
+
+def callback_input(in_data, frame_count, time_info, status):
+    datos.append(in_data)
+    return in_data, pyaudio.paContinue
+
+
 # %%
 code_path = '/home/juan/Documentos/Instrumentacion/instrumentacion'
-files_path = '/home/juan/Documentos/Instrumentacion'
+files_path = '/home/juan/Documentos/Instrumentacion/files'
 
-os.path.isfile(files_path)
+if ~os.path.isdir(code_path):
+    os.makedirs(files_path)
 
+# %%
 struct.pack('f', 3.141592654)
 struct.unpack('f', b'\xdb\x0fI@')
 struct.pack('4f', 1.0, 2.0, 3.0, 4.0)
@@ -46,11 +57,7 @@ struct.pack('4f', 1.0, 2.0, 3.0, 4.0)
 # %%
 
 pa = pyaudio.PyAudio()
-datos = [];
-def callback(in_data, frame_count, time_info, status):
-    datos.append(in_data)
-    return in_data, pyaudio.paContinue
-    
+datos = []
 ## Tomamos dia y hora actual para dar nombre al archivo wav
 #mes_dia = strftime("%m%d-%H_%M_%S")
 # Agregamos un contador para facilitar referir a los archivos
@@ -61,7 +68,7 @@ stream = pa.open(format=pyaudio.paInt16,
                  rate=44100,
                  input=True,
                  frames_per_buffer=1024,
-                 stream_callback=callback)
+                 stream_callback=callback_input)
 stream.start_stream()
 time.sleep(1)
 stream.stop_stream()
@@ -70,36 +77,32 @@ pa.terminate()
 # %%
 CHUNK = 1024
 
-volume = 0.51    # range [0.0, 1.0]
-fs = 44100       # sampling rate, Hz, must be integer
-duration = 1.0 # in seconds, may be float
-f = 2000      # sine frequency, Hz, may be float
- #   samples = volume*(np.cos(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
+volume = 1.    # range [0.0, 1.0]
+# Cambio sampling rate al valor maximo
+# En ubuntu se puede ver con cat /proc/asound/card0/codec#3
+fs = 192000       # sampling rate, Hz, must be integer
 
 # Barriendo en frecuencia
-frecuencia_inicial = 10 ;
-frecuencia_final = 20000 ;
-salto = 1000;
-frecuencias = np.arange(frecuencia_inicial,frecuencia_final,salto)
-n_periodos = 5 # cantidad de periodos
-
+frecuencia_inicial = 10
+frecuencia_final = 20000
+periodos_por_frec = 10
+n_frecuencias = 10
+frecuencias = np.geomspace(frecuencia_inicial, frecuencia_final, n_frecuencias)
+n_periodos = 10     # cantidad de periodos
 for freq in frecuencias:
     p = pyaudio.PyAudio()
-
     stream = p.open(format=pyaudio.paFloat32,
-                channels=1,
-                rate=fs,
-                output=True)
-    duration = 0.5
-    senial = volume * (np.sin(2*np.pi*np.arange(fs*duration)*freq/fs)).astype(np.float32)
-    
+                    channels=1,
+                    rate=fs,
+                    output=True)
+    duration = periodos_por_frec/freq
+    senial = senoidal(f_sampleo=fs, frecuencia=freq, duracion=duration,
+                      vpp=volume, dtype=np.float32)
     fin = CHUNK
-    
     while fin < len(senial):
         data = senial[fin-CHUNK:fin].tobytes()
         stream.write(data)
         fin += CHUNK
     stream.stop_stream()
     stream.close()
-
     p.terminate()
